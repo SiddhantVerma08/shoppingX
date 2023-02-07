3c57c96a2b24411ee4b20584806091f599f7d4b2
Siddhant <jointosid@gmail.com>
2023-02-07 21:13:21 +0530

    first commit

--- shoppingfy/settings.py
+++ shoppingfy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a=n%@m429^sj7&y)oa15w-@7gas6ti5wc5q9qtnsr0%=uyhn$k'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
